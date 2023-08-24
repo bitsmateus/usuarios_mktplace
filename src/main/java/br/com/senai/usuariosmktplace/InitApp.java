9817ce5721be17e27b067185a3f9f9cc33a1ec7f
mateus S B <mateus_s_bitencourt@estudante.sesisenai.org.br>
2023-08-23 22:05:22 -0300

    Concluido o service

--- src/main/java/br/com/senai/usuariosmktplace/InitApp.java
+++ src/main/java/br/com/senai/usuariosmktplace/InitApp.java
@@ -6,9 +6,6 @@ public class InitApp {
 
 	public static void main(String[] args) {
 		UsuarioService service = new UsuarioService();
-		System.out.println(service.removerAcentoDo("José da silva"));
-		System.out.println(service.fracionar("José da Silva Alburquerque dos Anjos e Bragança"));
-		System.out.println(service.gerarLoginPor("José da Silva dos Anjos"));
-		System.out.println(service.gerarHashDa("jose123456"));
+
 	}
 }
--- src/main/java/br/com/senai/usuariosmktplace/core/service/UsuarioService.java
+++ src/main/java/br/com/senai/usuariosmktplace/core/service/UsuarioService.java
@@ -7,6 +7,10 @@ import java.util.List;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.codec.digest.MessageDigestAlgorithms;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+
 import br.com.senai.usuariosmktplace.core.dao.DaoUsuario;
 import br.com.senai.usuariosmktplace.core.domain.Usuario;
 
@@ -19,11 +23,22 @@ public class UsuarioService {
 
 	}
 
-	public String removerAcentoDo(String nomeCompleto) {
+	public Usuario criarPor(String nomeCompleto, String senha) {
+		this.validar(nomeCompleto, senha);
+		String login = gerarLoginPor(nomeCompleto);
+		String senhaCriptografada = gerarHashDa(senha);
+		Usuario novoUsuario = new Usuario(login, senhaCriptografada, nomeCompleto);
+		this.dao.inserir(novoUsuario);
+		Usuario usuarioSalvo = dao.buscarPor(login);
+		return usuarioSalvo;
+
+	}
+
+	private String removerAcentoDo(String nomeCompleto) {
 		return Normalizer.normalize(nomeCompleto, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
 	}
 
-	public List<String> fracionar(String nomeCompleto) {
+	private List<String> fracionar(String nomeCompleto) {
 		List<String> nomeFracionado = new ArrayList<String>();
 
 		if (nomeCompleto != null && !nomeCompleto.isBlank()) {
@@ -44,7 +59,7 @@ public class UsuarioService {
 		return nomeFracionado;
 	}
 
-	public String gerarLoginPor(String nomeCompleto) {
+	private String gerarLoginPor(String nomeCompleto) {
 		nomeCompleto = removerAcentoDo(nomeCompleto);
 
 		List<String> partesDoNome = fracionar(nomeCompleto);
@@ -73,8 +88,30 @@ public class UsuarioService {
 		return loginGerado;
 	}
 
-	public String gerarHashDa(String senha) {
-		return new DigestUtils(MessageDigestAlgorithms.MD5).digestAsHex(senha);
+	private String gerarHashDa(String senha) {
+		return new DigestUtils(MessageDigestAlgorithms.SHA3_256).digestAsHex(senha);
+	}
+
+	private void validar(String senha) {
+		boolean isSenhaInvalida = Strings.isNullOrEmpty(senha) || senha.length() < 6 || senha.length() > 15;
+		Preconditions.checkArgument(isSenhaInvalida, "A senha é obrigatoria deve conter 6 e 15 caracteres");
+
+		boolean isContemLetra = CharMatcher.inRange('a', 'z').countIn(senha.toLowerCase()) > 0;
+		boolean isContemNumero = CharMatcher.inRange('0', '9').countIn(senha) > 0;
+		boolean isCaracterInvalido = !CharMatcher.javaLetterOrDigit().matchesAllOf(senha);
+
+		Preconditions.checkArgument(isCaracterInvalido && isContemNumero && !isCaracterInvalido,
+				"A sena deve conter letras e numeros");
+	}
+
+	private void validar(String nomeCompleto, String senha) {
+		List<String> partesDoNome = fracionar(nomeCompleto);
+		boolean isNomeCompleto = partesDoNome.size() > 1;
+		boolean isNomeValido = Strings.isNullOrEmpty(nomeCompleto) && isNomeCompleto && nomeCompleto.length() >= 5
+				&& nomeCompleto.length() <= 120;
+		Preconditions.checkArgument(isNomeValido,
+				"O nome é obrigatorio e deve conter entre 5 a 120 caracteres e conter sobrenome também");
+		this.validar(senha);
 	}
 
 }
